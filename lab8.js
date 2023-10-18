3632257b776b4ae73d2575a9acc6958441e9dab6
BrandonEvins <111911993+BrandonEvins@users.noreply.github.com>
2023-10-17 20:10:20 -0500

    Update lab8.js

--- lab8.js
+++ lab8.js
@@ -3,7 +3,7 @@ function getDataFromForm() {
   
   var firstName = document.querySelector('input[name="fname"]').value;
   var lastName = document.querySelector('input[name="lname"]').value;
-  alert(firstName+lastName);
+  
   runAjax(firstName, lastName);
 }
 
@@ -13,7 +13,7 @@ var xhttp = new XMLHttpRequest();
     var url = "myhomepage-sgn.azurewebsites.net/ajax.php";
     var fullURL = url + "?fname=" + encodeURIComponent(fname) + "&lname=" + encodeURIComponent(lname);
     xhttp.open("GET", fullURL, true);
-
+    alert(fullURL);
     xhttp.onreadystatechange = function () {
         if (xhttp.readyState === 4 && xhttp.status === 200) {
             document.getElementById("responseString").textContent = xhttp.responseText;
