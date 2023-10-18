5b4c9c581723e3e6ed89abec029b1366d851b228
BrandonEvins <111911993+BrandonEvins@users.noreply.github.com>
2023-10-17 20:14:34 -0500

    Update lab8.js

--- lab8.js
+++ lab8.js
@@ -10,10 +10,10 @@ function getDataFromForm() {
 function runAjax(fname, lname) {
 var xhttp = new XMLHttpRequest();
 
-    var url = "https://myhomepage-sgn.azurewebsites.net/ajax.php";
+    var url = "./ajax.php";
     var fullURL = url + "?fname=" + encodeURIComponent(fname) + "&lname=" + encodeURIComponent(lname);
     xhttp.open("GET", fullURL, true);
-    alert(fullURL);
+    
     xhttp.onreadystatechange = function () {
         if (xhttp.readyState === 4 && xhttp.status === 200) {
             document.getElementById("responseString").textContent = xhttp.responseText;
