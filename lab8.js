055a4307f99847a7825ce971f625609ee7e4bbfc
BrandonEvins <111911993+BrandonEvins@users.noreply.github.com>
2023-10-17 20:20:16 -0500

    Update lab8.js

--- lab8.js
+++ lab8.js
@@ -13,12 +13,14 @@ var xhttp = new XMLHttpRequest();
     var url = "https://myhomepage-sgn.azurewebsites.net/ajax.php";
     var fullURL = url + "?fname=" + encodeURIComponent(fname) + "&lname=" + encodeURIComponent(lname);
     xhttp.open("GET", fullURL, true);
-    
+    console.log("opened");
     xhttp.onreadystatechange = function () {
         if (xhttp.readyState === 4 && xhttp.status === 200) {
+          console.log("in 'onreadystatechange'");
             document.getElementById("responseString").textContent = xhttp.responseText;
         }
     };
-
+    console.log("Before open");
     xhttp.send();
+    console.log("After open");
 }
