60715174d1408e3fc9b66f99f26ecc7ec10eda96
BrandonEvins <111911993+BrandonEvins@users.noreply.github.com>
2023-11-01 15:56:32 -0500

    hehe

---
+++ Lab10/save_data.php
@@ -0,0 +1,20 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $firstName = $_POST['firstName'] ?? '';
+    $lastName = $_POST['lastName'] ?? '';
+
+    if (!empty($firstName) && !empty($lastName)) {
+        $data = $firstName . ' ' . $lastName . "\n";
+        $file = 'data/private_data.txt'; // Path to a non-public directory
+
+        // Append data to the file
+        file_put_contents($file, $data, FILE_APPEND);
+
+        echo "Data saved successfully.";
+    } else {
+        echo "Please provide both first and last names.";
+    }
+} else {
+    echo "Invalid request method.";
+}
+?>
---
+++ Lab10/show_data.php
@@ -0,0 +1,22 @@
+<?php
+$file = 'data/private_data.txt'; // Path to the same non-public directory
+
+if (file_exists($file)) {
+    $fileContents = file($file); // Read file into an array
+
+    echo '<table border="1">';
+    echo '<tr><th>First Name</th><th>Last Name</th></tr>';
+    
+    foreach ($fileContents as $line) {
+        $names = explode(' ', $line);
+        echo '<tr>';
+        echo '<td>' . $names[0] . '</td>';
+        echo '<td>' . $names[1] . '</td>';
+        echo '</tr>';
+    }
+
+    echo '</table>';
+} else {
+    echo "File not found.";
+}
+?>
